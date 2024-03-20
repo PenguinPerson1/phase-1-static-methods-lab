3ea67e1adb4bec7135e1e9e79f9a353e43655b73
Annania Plossl <nia.plossl@gmail.com>
2024-03-20 12:06:56 -0400

    completed the lab

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  static capitalize(word){
+    return word[0].toUpperCase() + word.slice(1);
+  }
+  static sanitize(word){
+    return word.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+  static titleize(sentence){
+    sentence = this.capitalize(sentence)
+    const words  = sentence.split(" ")
+    words.forEach((word, i) => {
+      if(word === "the" || word === "a" || word === "an" || word === "but" || word === "of" || word === "and" || word === "for" || word === "at" || word === "by" || word === "from"){
+
+      } else{
+        words[i] = this.capitalize(word);
+      }
+    });
+    return words.join(" ");
+  }
 }
